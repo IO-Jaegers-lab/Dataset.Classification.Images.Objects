36ca45d2072bb0480aeed5febd8e0a96e0ddc8cf
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2023-08-24 09:24:31 +0200

    removed
    Signed-off-by: Kent Madsen <kent.vejrup.madsen@designermadsen.dk>

--- hermes_dataset.py
+++
@@ -1,7 +0,0 @@
-from hermes_dataset_settings \
-    import HermesDatasetSettings
-
-
-class HermesDataset:
-    def __init__(self):
-        pass
--- interactive.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print('test')
-    print(__file__)
-
-
-if __name__ == '__main__':
-    main()
